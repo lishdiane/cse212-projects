7c4504d1ea67fdae3b6c3a38698a97eaffa265fe
lishdiane <dlish@byupathway.edu>
2025-10-30 10:45:10 -0600

    individual activity

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -1,8 +1,10 @@
-public static class Divisors {
+public static class Divisors
+{
     /// <summary>
     /// Entry point for the Divisors class
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         List<int> list = FindDivisors(80);
         Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
         List<int> list1 = FindDivisors(79);
@@ -16,9 +18,19 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
+    private static List<int> FindDivisors(int number)
+    {
         List<int> results = new();
-        // TODO problem 1
+
+        for (int divisor = 1; divisor < number; ++divisor)
+        {
+            int remainder = number % divisor;
+            if (remainder == 0)
+            {
+                results.Add(divisor);
+            }
+        }
+
         return results;
     }
 }
