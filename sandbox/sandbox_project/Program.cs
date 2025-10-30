e44082c9aeaf33ba445096c0ed863beb5e66c87a
lishdiane <dlish@byupathway.edu>
2025-10-30 16:57:43 -0600

    Week01 Code: Dynamic Arrays

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,13 +1,12 @@
 using System;
+using System.Reflection;
+using System.Runtime;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
-
+        
         Console.WriteLine("Hello Sandbox World!");
     }
 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,23 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // ----- Explanation ----- 
+        // First I will create an instance of a multiplier that is set to 1 
+        // Then I will make an empty array set to length
+        // I will make a for loop with i < length as the condition.
+        // In the for loop, number will be multiplied by the multiplier and added to the array.
+        // Finally the multiplier will be incremented by 1. 
+
+        var multiplier = 1;
+        double[] results = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = number * multiplier;
+            multiplier++;
+        }
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +45,27 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // ----- Explanation -----
+        // First I will create an copy of the array to hold the new positions and their values;
+        // Then I will make a for loop with i < length of the array as the condition. 
+        // In the for loop I will determine the new position by using remainders.
+        // I will set the item to its new position in the copy.
+        // Finally, I will set the copy array to the data array.
+
+        var newList = data.ToList();
+
+        for (int i = 0; i < data.Count; i++)
+
+        {
+            int position = (i + amount) % data.Count;
+            newList[position] = data[i];
+
+        }
+        
+        for (int i = 0; i < newList.Count; i++)
+        {
+            data[i] = newList[i];
+        }
     }
 }
