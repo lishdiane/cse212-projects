2023a0378b61e10ac8ca3df1f58aa68f5ea58695
lishdiane <dlish@byupathway.edu>
2025-11-19 10:26:58 -0700

    W04 Code: Linked Lists

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -6,7 +6,29 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
-        Console.WriteLine("Hello Sandbox World!");
+        var setA = new HashSet<int> { 1, 2, 3, 4 };
+        var setB = new HashSet<int> { 3, 4, 7, 9 };
+
+        var newSet = new HashSet<int>();
+
+        foreach (int number in setA)
+        {
+            if (setB.Contains(number))
+            {
+                newSet.Add(number);
+            }
+        }
+        Console.WriteLine(string.Join(", ", newSet));
+
+        var newUnion = new HashSet<int>(setA);
+
+        foreach (int item in setB)
+        {
+            if (!newUnion.Contains(item))
+            {
+                newUnion.Add(item);
+            }
+        }
+        Console.WriteLine(string.Join(", ", newUnion));
     }
 }
