193dafdc11e7d0fbb2f762a3e0637ed90fd3876a
lishdiane <dlish@byupathway.edu>
2025-12-06 13:53:26 -0700

    W06 Code Problems 1-4

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public class Node
 {
     public int Data { get; set; }
@@ -12,34 +14,73 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
-        if (value < Data)
+        if (value != Data)
         {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            if (value < Data)
+            {
+                Debug.WriteLine(Data);
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value != Data)
+        {
+            if (value < Data)
+            {
+                // Search to the left
+                if (Left is null)
+                    return false;
+                else
+                    return Left.Contains(value);
+            }
+            else
+            {
+                // Search to the right
+                if (Right is null)
+                    return false;
+                else
+                    return Right.Contains(value);
+            }
+        }
+        return true;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+        
+        if (Left == null && Right == null)
+        {
+            return 1;
+        }
+
+        if (Left != null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+
+        if (Right != null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
